66d72d4e62406332f253b53e270d63fb4e9ce5bc
Mikko Hirvonen <hirvonenmp@gmail.com>
2017-01-20 11:42:14 +0200

    Harjoitus2

--- Teht01/Program.cs
+++ Teht01/Program.cs
@@ -10,7 +10,20 @@ namespace Teht01
     {
         static void Main(string[] args)
         {
-          
+            int luku;
+            Console.Write("Anna numero > ");
+            string line = Console.ReadLine();
+            luku = int.Parse(line);
+            switch (luku)
+            {
+                case 1: Console.Write("yksi"); break;
+                case 2: Console.Write("kaksi"); break;
+                case 3: Console.Write("kolme"); break;
+                default: Console.Write("Joku muu luku"); break;
+
+            }
+            Console.ReadLine();
+
         }
     }
 }
